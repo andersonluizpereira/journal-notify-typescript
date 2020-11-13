a5805030a06f9854340e7af27b4c78c2122410a0
Anderson Pereira <andersononus@hotmail.com>
2020-11-13 19:07:51 -0300

    feat: add test facktory mock-authentication

---
+++ src/domain/test/mock-authentication/mock-authentication.ts
@@ -0,0 +1,7 @@
+import { AuthenticationParams } from "@/domain/usecases";
+import faker from 'faker'
+
+export const mockAuthenticationParams = (): AuthenticationParams => ({
+    email: faker.internet.email(),
+    password: faker.internet.password()
+  })
