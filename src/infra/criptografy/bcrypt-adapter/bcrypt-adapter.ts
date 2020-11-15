e9daceb4ace748da22183f2956360a8f3ec7fcc5
Anderson Pereira <andersononus@hotmail.com>
2020-11-15 17:56:21 -0300

    feat: add validator e-mail, cryptografy, jwt-adapter

---
+++ src/infra/criptografy/bcrypt-adapter/bcrypt-adapter.ts
@@ -0,0 +1,16 @@
+import { HashComparer, Hasher } from '@/data/protocols/criptography'
+import bcrypt from 'bcrypt'
+
+export class BcryptAdapter implements Hasher, HashComparer {
+  constructor (private readonly salt: number) {}
+
+  async hash (plaintext: string): Promise<string> {
+    const digest = await bcrypt.hash(plaintext, this.salt)
+    return digest
+  }
+
+  async compare (plaintext: string, digest: string): Promise<boolean> {
+    const isValid = await bcrypt.compare(plaintext, digest)
+    return isValid
+  }
+}
---
+++ src/infra/validators/email-validator-adapter.ts
@@ -0,0 +1,7 @@
+import { EmailValidator } from '@/validation/protocols/email-validator'
+import validator from 'validator'
+export class EmailValidatorAdapter implements EmailValidator {
+  isValid (email: string): boolean {
+    return validator.isEmail(email)
+  }
+}
