5c5ba490ffe7c7b9c8e9f4798153267bda47690d
Anderson Pereira <andersononus@hotmail.com>
2020-11-22 18:27:02 -0300

    feat: modify makeDbAddAccount adjust AccountsRepository typeorm

--- src/main/factories/usecases/account/add-account/db-add-account-factory.ts
+++ src/main/factories/usecases/account/add-account/db-add-account-factory.ts
@@ -1,11 +1,11 @@
-import { AccountMongoRepository } from '@/infra/db/mongodb/account/account-mongo-repository'
 import { AddAccount } from '@/domain/usecases/account/add-account'
 import { BcryptAdapter } from '@/infra/criptografy/bcrypt-adapter/bcrypt-adapter'
 import { DbAddAccount } from '@/data/usecases'
+import { AccountsRepository } from '@/infra/db/typeorm/repositories/AccountsRepository'
 
 export const makeDbAddAccount = (): AddAccount => {
   const salt = 12
   const bcryptAdapter = new BcryptAdapter(salt)
-  const accountMongoRepository = new AccountMongoRepository()
-  return new DbAddAccount(bcryptAdapter, accountMongoRepository, accountMongoRepository)
+  const accountsRepository = new AccountsRepository()
+  return new DbAddAccount(bcryptAdapter, accountsRepository, accountsRepository)
 }
--- src/main/server.ts
+++ src/main/server.ts
@@ -1,10 +1,14 @@
-import 'module-alias/register'
-import { MongoHelper } from '@/infra/db/mongodb/helpers/mongo-helper'
 import env from './config/env'
+import 'reflect-metadata'
+import createConnection from '@/infra/db/typeorm/connection'
+import { MongoHelper } from '@/infra/db/mongodb/helpers'
 
 MongoHelper.connect(env.mongoUrl)
   .then(async () => {
-    const app = (await import('./config/app')).default
-    app.listen(env.port, () => console.log(`Server running at http://localhost:${env.port}`))
+    // eslint-disable-next-line @typescript-eslint/no-floating-promises
+    createConnection().then(async () => {
+      const app = (await import('./config/app')).default
+      app.listen(env.port, () => console.log(`Server running at http://localhost:${env.port}`))
+    })
   })
   .catch(console.error)
