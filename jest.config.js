7d1abf08890afbb3567e192a6a88434be1c5fe4c
Anderson Pereira <andersononus@hotmail.com>
2020-11-27 16:06:30 -0300

    feat:adjust config jest

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,9 @@ module.exports = {
     '<rootDir>/src/**/*.ts',
     '!<rootDir>/src/main/**',
     '!<rootDir>/src/**/*-protocols.ts',
+    '!<rootDir>/src/infra/criptografy/jwt-adapter/*-adapter.ts',
+    '!<rootDir>/src/infra/db/typeorm/migrations/*.ts',
+    '!<rootDir>/src/infra/db/typeorm/connection.ts',
     '!<rootDir>/src/**/index.ts',
     '!**/protocols/**',
     '!**/test/**'
