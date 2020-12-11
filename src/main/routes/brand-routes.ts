f55b9f03f8544a9585defe24bedfb428db799252
Anderson Pereira <andersononus@hotmail.com>
2020-12-11 19:06:54 -0300

    feat: created route allbrands

--- src/main/routes/brand-routes.ts
+++ src/main/routes/brand-routes.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express'
 import { adaptRoute } from '../adapters/express-routes-adapter'
 import { makeAddBrandController } from '../factories/controllers/brand/add-brand/add-brand-controller-factory'
+import { makeLoadBrandsController } from '../factories/controllers/brand/load-brands/load-brands-controller-factory'
 import { adminAuth } from '../middlewares/admin-auth'
 
 export default (router: Router): void => {
   router.post('/brands', adminAuth, adaptRoute(makeAddBrandController()))
+  router.get('/brands', adaptRoute(makeLoadBrandsController()))
 }
---
+++ src/presentation/controllers/brand/load-brands/load-brands-controller-protocols.ts
@@ -0,0 +1,3 @@
+export * from '../../../protocols'
+export * from '@/domain/usecases/brand/load-brands'
+export * from '@/domain/models/brand/brand'
