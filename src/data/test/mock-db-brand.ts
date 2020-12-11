06f002b6edddfa815db8ed62a67c3e78732c71dd
Anderson Pereira <andersononus@hotmail.com>
2020-12-11 19:21:55 -0300

    test: dbloadbrands

--- src/data/test/mock-db-brand.ts
+++ src/data/test/mock-db-brand.ts
@@ -2,7 +2,8 @@ import { AddBrandRepository } from '../protocols/db'
 import { AddBrandParams } from '@/domain/usecases/brand/add-brand'
 import { LoadBrandByIdRepository } from '../protocols/db/brand/load-brand-by-id-repository'
 import { BrandModel } from '@/domain/models/brand/brand'
-import { mockBrandModel } from '@/domain/test/mock-brand/mock-brand'
+import { mockBrandModel, mockBrandModels } from '@/domain/test/mock-brand/mock-brand'
+import { LoadBrandsRepository } from '../usecases/brand/load-brand/db-load-brand-protocols'
 
 export class AddBrandRepositorySpy implements AddBrandRepository {
   addBrandParams: AddBrandParams
@@ -20,5 +21,13 @@ export class LoadBrandByIdRepositorySpy implements LoadBrandByIdRepository {
   async loadById (id: string): Promise<BrandModel> {
     this.id = id
     return Promise.resolve(this.brandModel)
+  }
+}
+
+export class LoadBrandsRepositorySpy implements LoadBrandsRepository {
+  brandModels = mockBrandModels()
+
+  async loadAll (): Promise<BrandModel[]> {
+    return Promise.resolve(this.brandModels)
   }
 }
