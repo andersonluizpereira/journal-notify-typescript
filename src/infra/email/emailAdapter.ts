5377c2fd17c0f84851f358b9cb4eda0b4354b537
Anderson Pereira <andersononus@hotmail.com>
2020-12-22 16:03:05 -0300

    feat: add email adapter

---
+++ src/infra/email/emailAdapter.ts
@@ -0,0 +1,21 @@
+import { eMail, emailParams } from '@/data/protocols/email/email'
+import nodemailer, { Transporter } from 'nodemailer'
+
+export class EmailAdapter implements eMail {
+  constructor (private readonly client: Transporter) {}
+
+  async sendEmail ({ to, subject, html }: emailParams): Promise<void> {
+    const info = await this.client.sendMail({
+      from: {
+        name: 'Jounal',
+        address: 'journalcarlucci@mailinator.com'
+      },
+      to,
+      subject,
+      html
+    })
+
+    console.log('Message sent: %s', info.messageId)
+    console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info))
+  }
+}
